32896e9ce69f1ac6c08fa5e8bcc18a9fbd7e4fbe
mhirrt12 <kaletamene90@gmail.com>
2026-07-25 05:59:56 -0700

    fix: correct formatting and indentation in addItem and removeItem reducers

--- redux-cart/src/features/cart/cartSlice.js
+++ redux-cart/src/features/cart/cartSlice.js
@@ -8,20 +8,30 @@ import { createSlice } from '@reduxjs/toolkit';
 const cartSlice = createSlice({
     name: 'cart',
     initialState,
-    reducers:{
-   addItem(state,action){
-      const existingItem = state.cartItems.find((item)=> item.id === action.payload.id);
-      if(existingItem){
-         existingItem.quantity +=1;
-      } else {
-         state.cartItems.push({
-         ...action.payload,
-         quantity: 1
-         });;
-      }
+    reducers: {
+        addItem(state, action) {
+            const existingItem = state.cartItems.find((item) => item.id === action.payload.id);
+            if (existingItem) {
+                existingItem.quantity += 1;
+            } else {
+                state.cartItems.push({
+                    ...action.payload,
+                    quantity: 1
+                });;
+            }
             state.totalQuantity += 1;
             state.totalAmount += action.payload.price;
             
-   }
-}},
-); 
+        },
+        removeItem(state, action) {
+            const existingItem = state.cartItems.find((item) => item.id === action.payload.id);
+            if (existingItem.quantity === 1) {
+                state.cartItems = state.cartItems.filter((item) => item.id !== action.payload.id);
+            } else {
+                existingItem.quantity -= 1;
+            }
+            state.totalQuantity -= 1;
+            state.totalAmount -= action.payload.price;
+        }
+    },
+}); 
