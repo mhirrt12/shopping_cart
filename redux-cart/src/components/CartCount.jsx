4c2d6cafe9d996b968ea945a868e446c8a5f3c4f
mhirrt12 <kaletamene90@gmail.com>
2026-07-25 06:48:56 -0700

    fix: correct total quantity selector in CartCount component

--- redux-cart/src/components/CartCount.jsx
+++ redux-cart/src/components/CartCount.jsx
@@ -4,10 +4,11 @@ const CartCount = () => {
     const cartItems = useSelector((state) => state.cart.cartItems);
 
     const totalamount = useSelector((state) => state.cart.totalAmount);
-    const totalQuantitiy = useSelector((state) => state.cart.totalAmount);
+    const totalQuantitiy = useSelector((state) => state.cart. totalQuantity);
     return <>
         <h1>items in cart: {cartItems.length}</h1>;
         <h1>total amount : {totalamount}</h1>;
+          <h1>total quantity : {totalQuantitiy}</h1>;
     </>
 }
 export default CartCount;
