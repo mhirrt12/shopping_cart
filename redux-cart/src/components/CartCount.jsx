6034679a9c7050692882e45c4a1d580c9a8179a6
mhirrt12 <kaletamene90@gmail.com>
2026-07-25 06:47:16 -0700

    fix: correct variable name for total quantity in CartCount component

--- redux-cart/src/components/CartCount.jsx
+++ redux-cart/src/components/CartCount.jsx
@@ -3,7 +3,8 @@ import { useSelector } from "react-redux";
 const CartCount = () => {
     const cartItems = useSelector((state) => state.cart.cartItems);
 
-     const totalamount = useSelector((state) => state.cart.totalAmount);
+    const totalamount = useSelector((state) => state.cart.totalAmount);
+    const totalQuantitiy = useSelector((state) => state.cart.totalAmount);
     return <>
         <h1>items in cart: {cartItems.length}</h1>;
         <h1>total amount : {totalamount}</h1>;
