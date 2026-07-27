41097decf253a44fa1ad846f40e653ee7283c781
mhirrt12 <kaletamene90@gmail.com>
2026-07-27 10:30:28 -0700

    fix: add missing semicolon in store configuration

--- redux-cart/src/app/store.js
+++ redux-cart/src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../features/cart/cartSlice";
+import productReducer from "../features/products/productSlice"
 
 export const store = configureStore({
     reducer: {
