faf3829ab4679176842e14964e24793f15e6595e
mhirrt12 <kaletamene90@gmail.com>
2026-07-27 08:51:00 -0700

    fix: streamline state selection in CartCount component

--- redux-cart/src/components/CartCount.jsx
+++ redux-cart/src/components/CartCount.jsx
@@ -1,14 +1,17 @@
 import { useSelector } from "react-redux";
 
 const CartCount = () => {
-    const cartItems = useSelector((state) => state.cart.cartItems);
+    const { cartItems, totalAmount, totalQuantity } = useSelector((state) => state.cart);
 
-    const totalamount = useSelector((state) => state.cart.totalAmount);
-    const totalQuantitiy = useSelector((state) => state.cart. totalQuantity);
+    // const totalamount = useSelector((state) => state.cart.totalAmount);
+    // const totalQuantitiy = useSelector((state) => state.cart. totalQuantity);
     return <>
         <h1>items in cart: {cartItems.length}</h1>;
-        <h1>total amount : {totalamount}</h1>;
-          <h1>total quantity : {totalQuantitiy}</h1>;
+        <h1>total amount : {totalAmount}</h1>;
+        <h1>total quantity : {totalQuantity}</h1>;
+        {/* {cartItems.map((items) => {
+            
+        })} */}
     </>
 }
 export default CartCount;
