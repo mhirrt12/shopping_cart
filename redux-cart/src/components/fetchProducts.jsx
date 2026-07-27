1d58b0aeaed49789ed37eeb5c44c139fef94e448
mhirrt12 <kaletamene90@gmail.com>
2026-07-27 09:43:09 -0700

    feat: implement fetchProducts async thunk for product retrieval

---
+++ redux-cart/src/components/fetchProducts.jsx
@@ -0,0 +1,14 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchProducts = createAsyncThunk(
+    "products/fetchProducts",
+    async () => {
+        const response = await fetch(
+            "https://fakestoreapi.com/products"
+        );
+
+        const data = await response.json();
+
+        return data;
+    }
+);
