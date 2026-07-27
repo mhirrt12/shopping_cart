fb621298f3bf124fff467647a4ae584d61dbd728
mhirrt12 <kaletamene90@gmail.com>
2026-07-27 10:26:08 -0700

    fix: add productReducer to store configuration

--- redux-cart/src/app/store.js
+++ redux-cart/src/app/store.js
@@ -3,6 +3,7 @@ import cartReducer from "../features/cart/cartSlice";
 
 export const store = configureStore({
     reducer: {
-        cart: cartReducer
+         products: productReducer, 
+        cart: cartReducer,
     }
 });
