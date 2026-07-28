ddcdcb46aea4b954abc7dc926d2fd4d2c18513e1
mhirrt12 <kaletamene90@gmail.com>
2026-07-28 03:06:12 -0700

    refactor: clean up Products component structure and formatting

--- redux-cart/src/components/Products.jsx
+++ redux-cart/src/components/Products.jsx
@@ -2,18 +2,30 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../features/products/productThunk";
 
 const Products = () => {
+    const dispatch = useDispatch();
 
-const dispatch = useDispatch();
+    const { products, loading, error } = useSelector(
+        (state) => state.products
+    );
 
-const {products, loading, error} =
-useSelector(state => state.products);
+    return (
+        <>
+            <button onClick={() => dispatch(fetchProducts())}>
+                Load Products
+            </button>
 
+            {loading && <h2>Loading...</h2>}
 
-return (
-<button onClick={()=>dispatch(fetchProducts())}>
-Load Products
-</button>
-)
+            {error && <h2>{error}</h2>}
 
-}
+            {products.map((product) => (
+                <div key={product.id}>
+                    <h3>{product.title}</h3>
+                    <p>${product.price}</p>
+                </div>
+            ))}
+        </>
+    );
+};
+
 export default Products;
